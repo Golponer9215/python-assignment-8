7838b14fc77f309075564fbdfcde62888868ceff
Golponer9215 <nja71764@gmail.com>
2025-09-05 03:58:36 +0100

    added bank.py

--- bank.py
+++ bank.py
@@ -13,3 +13,26 @@ Example Usage:
     acc.withdraw(200)
     print(acc.get_balance())  # 1300
 """
+class BankAccount:
+     def __init__(self, account_name, starting_balance):
+        self.account_name = account_name
+        self.balance = starting_balance
+
+     def deposit(self, amount):
+        self.balance += amount
+
+     def withdraw(self, amount):
+        if amount > self.balance:
+            print("Insufficient balance")
+        else:
+            self.balance -= amount
+
+     def get_balance(self):
+        return self.balance
+
+acc1 = BankAccount("Gotom", 100000)
+acc2= BankAccount ("Nenny",50000)
+acc1.deposit(5000)
+acc2.withdraw(2000)
+print(acc1.get_balance())
+print(acc2.get_balance())
