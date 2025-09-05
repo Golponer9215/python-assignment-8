69e32ff82bdb6f97f2fa33c34362a6c211f67398
Golponer9215 <nja71764@gmail.com>
2025-09-05 05:33:21 +0100

    added library.py

--- library.py
+++ library.py
@@ -19,3 +19,46 @@ Example Usage:
     lib.return_book("Alice")
     print(lib.show_available_books())  # ["Data Science Handbook", "Python 101"]
 """
+class Library:
+    def __init__(self):
+        self.available_books = []
+        self.borrowed_books = {}
+
+    def add_book(self, book_title):
+        """Add a new book to the library."""
+        self.available_books.append(book_title)
+
+    def borrow_book(self, user, book_title):
+        """Allow a user to borrow a book if available."""
+        if book_title in self.available_books:
+            self.available_books.remove(book_title)
+            self.borrowed_books[user] = book_title
+            print(f"{user} borrowed '{book_title}'.")
+        else:
+            print(f"Sorry, '{book_title}' is not available.")
+
+    def return_book(self, user):
+        """Allow a user to return a borrowed book."""
+        if user in self.borrowed_books:
+            book_title = self.borrowed_books[user]
+            self.available_books.append(book_title)
+            del self.borrowed_books[user]
+            print(f"{user} returned '{book_title}'.")
+        else:
+            print(f"{user} has no borrowed books.")
+
+    def show_available_books(self):
+        """Return a list of available books."""
+        return self.available_books
+
+
+
+lib = Library()
+lib.add_book("Python 101")
+lib.add_book("Data Science Handbook")
+
+lib.borrow_book("Alice", "Python 101")
+print(lib.show_available_books()) 
+
+lib.return_book("Alice")
+print(lib.show_available_books())  
